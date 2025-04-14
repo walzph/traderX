2ccfaf9c90392b7f7b62931dc621750f5d5939c8
Jonas Zipprick <jonas@codesphere.com>
2025-04-14 11:45:44 +0000

    fix trade processor

--- web-front-end/angular/main/environments/environment.prod.ts
+++ web-front-end/angular/main/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
     production:         true,
     accountUrl:         `//${window.location.host}`,
-    refrenceDataUrl:    `//ws-server-66268-reference-data.workspaces:3000`,
-    tradesUrl:          `//ws-server-66268-trade-processor.workspaces:3000`,
-    positionsUrl:       `//${window.location.host}/position-service`,
-    peopleUrl:          `//${window.location.host}/people-service`,
-    tradeFeedUrl:       `//${window.location.host}`
+    refrenceDataUrl:    `//${window.location.host}`,
+    tradesUrl:          `//${window.location.host}/trade`,
+    positionsUrl:       `//${window.location.host}`,
+    peopleUrl:          `//${window.location.host}`,
+    tradeFeedUrl:       `//${window.location.host}/trade-feed`
 };
--- web-front-end/angular/main/environments/environment.ts
+++ web-front-end/angular/main/environments/environment.ts
@@ -5,11 +5,11 @@
 export const environment = {
     production:         true,
     accountUrl:         `//${window.location.host}`,
-    refrenceDataUrl:    `//ws-server-66268-reference-data.workspaces:3000`,
-    tradesUrl:          `//ws-server-66268-trade-processor.workspaces:3000`,
+    refrenceDataUrl:    `//${window.location.host}`,
+    tradesUrl:          `//${window.location.host}/trade`,
     positionsUrl:       `//${window.location.host}`,
     peopleUrl:          `//${window.location.host}`,
-    tradeFeedUrl:       `//${window.location.host}`
+    tradeFeedUrl:       `//${window.location.host}/trade-feed`
 };
 
 /*
